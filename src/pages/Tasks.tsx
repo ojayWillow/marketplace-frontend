b9d12aa4117dba66c32f5b0d910b53ccbb1d480d
ojayWillow <og.vitols@gmail.com>
2026-01-02 20:10:35 +0200

    fix: Remove CircleMarker import to resolve syntax error

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup, CircleMarker } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { Icon } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
