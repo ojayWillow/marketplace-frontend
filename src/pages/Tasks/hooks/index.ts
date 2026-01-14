8f2c865424da9ea3304d80abf0703bf7c8e72dd0
ojayWillow <og.vitols@gmail.com>
2026-01-14 07:49:30 +0200

    chore: Update hooks index to export all task hooks

--- src/pages/Tasks/hooks/index.ts
+++ src/pages/Tasks/hooks/index.ts
@@ -1,4 +1,9 @@
-// Custom hooks barrel export
-export * from './useTaskData';
-export * from './useTaskFilters';
-export * from './useLocation';
+// Task hooks - extracted from Tasks.tsx
+export { useTaskLocation } from './useTaskLocation';
+export type { UseTaskLocationReturn } from './useTaskLocation';
+
+export { useTaskData } from './useTaskData';
+export type { UseTaskDataParams, UseTaskDataReturn } from './useTaskData';
+
+export { useTaskFilters, DEFAULT_FILTERS } from './useTaskFilters';
+export type { UseTaskFiltersParams, UseTaskFiltersReturn } from './useTaskFilters';
