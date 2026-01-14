51f91becf178bcc5f1ca2ad041c9ce92cb3c2276
ojayWillow <og.vitols@gmail.com>
2026-01-14 07:57:15 +0200

    fix: Pass searchRadius prop to MapLoadingOverlay for contextual message

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -311,7 +311,7 @@ const DesktopTasksView = () => {
           
           {/* Map Container */}
           <div className="relative" style={{ height: '350px' }}>
-            <MapLoadingOverlay isLoading={refreshing} />
+            <MapLoadingOverlay isLoading={refreshing} searchRadius={searchRadius} />
             <MapContainer center={[userLocation.lat, userLocation.lng]} zoom={13} style={{ height: '100%', width: '100%' }}>
               <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
               <MapMarkers
