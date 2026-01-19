1a5aec995c1cf8b11a4e709cf50212a134df25ff
ojayWillow <og.vitols@gmail.com>
2026-01-19 20:59:58 +0200

    refactor: Update ProtectedRoute to use shared package

--- apps/web/src/components/ProtectedRoute.tsx
+++ apps/web/src/components/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate, useLocation } from 'react-router-dom'
-import { useAuthStore } from '../stores/authStore'
+import { useAuthStore } from '@marketplace/shared'
 
 interface ProtectedRouteProps {
   children: React.ReactNode
