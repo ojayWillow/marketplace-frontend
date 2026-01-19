66b6e7d48304791b3da6c1e7f63e80012503a3d8
ojayWillow <og.vitols@gmail.com>
2026-01-19 20:03:18 +0200

    chore: Add ESLint config for React Native mobile app

---
+++ apps/mobile/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  extends: [
+    'expo',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    'react-native/react-native': true,
+    es6: true,
+    node: true,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Not needed in React Native
+    'react/prop-types': 'off', // Using TypeScript
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
