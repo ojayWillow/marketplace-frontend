6678360f6ac5bcce73fa97d3561365ecb1851fde
ojayWillow <og.vitols@gmail.com>
2026-01-19 21:29:18 +0200

    fix: Import i18n from shared package

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
-import './i18n'
+import '@marketplace/shared/src/i18n' // Import i18n from shared package to initialize it
 import './index.css'
 
 const queryClient = new QueryClient({
