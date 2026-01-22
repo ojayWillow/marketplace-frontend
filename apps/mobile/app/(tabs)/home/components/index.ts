11b48069099159cfa992396a2f2de0d122a9c58d
ojayWillow <og.vitols@gmail.com>
2026-01-22 11:19:02 +0200

    chore: remove old component barrel file from app/ folder

--- apps/mobile/app/(tabs)/home/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { UserMarker } from './UserMarker';
-export { JobCard } from './JobCard';
-export { FocusedJob } from './FocusedJob';
-export { SearchBar } from './SearchBar';
-export { CategoryModal, RadiusModal } from './FilterModals';
-export { CreateModal } from './CreateModal';
