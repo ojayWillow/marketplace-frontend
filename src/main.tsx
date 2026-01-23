97150d2a3316389ad71913e095ab9460ff019d52
ojayWillow <og.vitols@gmail.com>
2026-01-23 17:42:19 +0200

    refactor: use shared queryClient from lib

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/queryClient'
 import App from './App'
 import './i18n'
 import './index.css'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      retry: 1,
-    },
-  },
-})
 
 // Service worker is registered by Vite PWA plugin
 // Push notification handlers are imported via workbox.importScripts in vite.config.ts
