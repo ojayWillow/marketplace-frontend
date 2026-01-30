b8e17352fe96122095d67f92e50c256c185a54c8
ojayWillow <og.vitols@gmail.com>
2026-01-30 18:01:21 +0200

    Add barrel export for home components

---
+++ apps/mobile/app/(tabs)/home/components/index.ts
@@ -0,0 +1,5 @@
+export { HomeSearchBar } from './HomeSearchBar';
+export { HomeCategoryButton } from './HomeCategoryButton';
+export { HomeFloatingControls } from './HomeFloatingControls';
+export { HomeMapMarkers } from './HomeMapMarkers';
+export { HomeBottomSheet } from './HomeBottomSheet';
