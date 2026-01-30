c69efcf47a83f6b89c5582bbf0ae45537fb255c0
ojayWillow <og.vitols@gmail.com>
2026-01-30 03:59:08 +0200

    feat: Add components index for easier imports
    Exports all reusable components from a single entry point.

---
+++ apps/mobile/components/index.ts
@@ -0,0 +1,12 @@
+// Layout & Utility Components
+export { KeyboardAwareContainer } from './KeyboardAwareContainer';
+
+// UI Components
+export { default as CachedImage } from './CachedImage';
+export { default as ImagePicker } from './ImagePicker';
+export { default as LocationPicker } from './LocationPicker';
+export { default as StarRating } from './StarRating';
+
+// Card Components
+export { default as TaskCard } from './TaskCard';
+export { default as OfferingCard } from './OfferingCard';
