8c62ae7e5f13040a5cc4b2b1a1cfb6df02c37bee
ojayWillow <og.vitols@gmail.com>
2026-01-31 18:49:46 +0200

    Add useCategories export to hooks index

--- apps/mobile/src/hooks/index.ts
+++ apps/mobile/src/hooks/index.ts
@@ -1,3 +1,8 @@
 // Activity & Notification Hooks
 export { useActivityCounts } from './useActivityCounts';
 export type { ActivityCounts } from './useActivityCounts';
+
+// Translation Hooks
+export { useTranslation } from './useTranslation';
+export { useCategories } from './useCategories';
+export type { TranslatedCategory } from './useCategories';
