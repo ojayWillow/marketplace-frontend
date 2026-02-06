a6328434046d369f4be8b15e9887c3242348e407
ojayWillow <og.vitols@gmail.com>
2026-02-06 07:58:51 +0200

    Export new minimal header components

--- apps/web/src/components/MobileTasksView/components/index.ts
+++ apps/web/src/components/MobileTasksView/components/index.ts
@@ -3,3 +3,6 @@ export { default as MobileJobCard } from './MobileJobCard';
 export { default as JobPreviewCard } from './JobPreviewCard';
 export { default as CreateChoiceModal } from './CreateChoiceModal';
 export { default as SlideOutMenu } from './SlideOutMenu';
+export { MinimalHeader } from './MinimalHeader';
+export { SearchOverlay } from './SearchOverlay';
+export { FilterSheet } from './FilterSheet';
