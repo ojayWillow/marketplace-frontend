c606ab4288cc9780fdd552407b579e58a17a8791
ojayWillow <og.vitols@gmail.com>
2026-02-16 12:35:59 +0200

    feat: add MobileFavoritesSection to mobile profile page

--- apps/web/src/pages/Profile/Profile.tsx
+++ apps/web/src/pages/Profile/Profile.tsx
@@ -25,6 +25,7 @@ import {
 import { MobileReviewsSection } from './components/mobile/MobileReviewsSection';
 import { MobileActivitySection } from './components/mobile/MobileActivitySection';
 import { MobileListingsTeaser } from './components/mobile/MobileListingsTeaser';
+import { MobileFavoritesSection } from './components/mobile/MobileFavoritesSection';
 import CommunityRulesModal from '../../components/QuickHelpIntroModal';
 
 const Profile = () => {
@@ -199,6 +200,8 @@ const Profile = () => {
                   </div>
                 ) : (
                   <>
+                    <MobileFavoritesSection />
+
                     <MobileReviewsSection
                       reviews={reviews}
                       showAll={showAllReviews}
