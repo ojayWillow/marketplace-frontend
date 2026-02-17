f5b2b625535832eeae30e098bbc4d4f2732a516b
ojayWillow <og.vitols@gmail.com>
2026-02-17 17:28:59 +0200

    chore: remove dead listings code and unused SlideOutMenu (#83)
    Remove deprecated listings feature files (pages, hooks, constants, components) and unused SlideOutMenu/MobileTasksView shim. All listing functionality was superseded by Tasks + Offerings.

--- apps/web/src/components/MobileTasksView/components/index.ts
+++ apps/web/src/components/MobileTasksView/components/index.ts
@@ -2,6 +2,5 @@ export { default as MapController } from './MapController';
 export { default as MobileJobCard } from './MobileJobCard';
 export { default as JobPreviewCard } from './JobPreviewCard';
 export { default as CreateChoiceModal } from './CreateChoiceModal';
-export { default as SlideOutMenu } from './SlideOutMenu';
 export { default as FilterSheet } from './FilterSheet';
 export { default as FloatingSearchBar } from './FloatingSearchBar';
