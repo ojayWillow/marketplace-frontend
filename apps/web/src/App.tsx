0c6fc378f4f655d6368777eb47fe7c95b137a4b8
ojayWillow <og.vitols@gmail.com>
2026-02-27 16:28:20 +0200

    feat: add Sentry ErrorBoundary to App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 import { lazy, Suspense } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
